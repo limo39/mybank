b63134bff95d36b6d8a692973493e4d91713c943
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-06-03 20:48:31 +0300

    Update mybank.c Windows

--- mybank.c
+++ mybank.c
@@ -1,103 +1,52 @@
-#include <stdio.h>
-#include <string.h>
+#include <windows.h>
 
-
-int main()
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	char option;
-	double balance = 50000.00;
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    const char CLASS_NAME[] = "MyBankWindowClass";
 
-	/* Greeting message */
-	printf("\nHello, Welcome to Mybank. With us, Banking has been  made easy. \n");
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
 
+    RegisterClass(&wc);
 
-	/* Create an account - Implementing registration */
-	char firstname[50], lastname[50];
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        "My Bank",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
 
-	printf("Please create an account \nEnter your first and last names here: ");
-	scanf("%s %s", firstname, lastname);
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
 
-	char pass[20];
-	char val[20];
-	int value;
-	int i = 3;
-	int passwordCorrect = 0; //Flag var to track correct password validation.
+    ShowWindow(hwnd, nCmdShow);
 
-	printf("Create a passowrd: ");
-	scanf("%s", pass);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-	printf("Confirm  passowrd: ");
-	scanf("%s", val);
-
-	value = strcmp(pass, val);
-
-	if (value != 0)
-		while (i >= 0)
-		{
-			printf("\n Your passwords dont match \n");
-			printf("Try again, Re-enter password: ");
-			scanf("%s", val);
-
-			value = strcmp(pass, val);
-
-			if ( value == 0)
-			{
-				passwordCorrect = 1;
-				break;
-			}
-			if (i > 0)
-				printf("Passwords dont match. You have %d more attempts \n", i);
-			else
-				printf("Sorry. You cant create an account");
-			i--;
-		}
-	else
-		passwordCorrect = 1;
-
-	if (passwordCorrect)
-	{
-		printf("Welcome to instant banking with my bank. \nYou have a fixed amount of 50,000 \n \n");
-
-		while(1)
-		{
-			printf("Menu: \n");
-			printf("a) Transfer funds \n");
-			printf("b) Deposit funds \n");
-			printf("c) Check balance \n");
-			printf("d) Quit the program \n");
-
-			printf("Select an option: ");
-			scanf(" %c", &option);
-
-			switch (option)
-			{
-				case 'a':
-					printf("Option a: Transfer Money\n");
-					/* Add code to transfer money */
-					break;
-
-				case 'b':
-					printf("Option b: Deposit Money\n");
-					/* Add code to deposit money */
-					break;
-
-				case 'c':
-					printf("Option c: Check balance\n");
-					/* Add code to check balance */
-					break;
-
-				case 'd':
-					printf("Option d: Quit the program\n");
-					return 0;
-
-
-			}	
-
-
-		}
-
-	}
-
-	return 0;
+    return 0;
 }
