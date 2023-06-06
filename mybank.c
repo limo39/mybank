3e55ee9c85d11071c71483250fd17445b5242e47
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-06-06 04:18:09 +0300

    Update mybank.c

--- mybank.c
+++ mybank.c
@@ -2,35 +2,16 @@
 #include <string.h>
 #include <ncurses.h>
 
-int main()
+// Function to create an account and validate the password
+int createAccount(WINDOW *win)
 {
-    char option;
-    double balance = 50000.00;
-
-    /* Initialize ncurses */
-    initscr();
-    cbreak();
-    noecho();
-
-    /* Create a new window */
-    WINDOW *win = newwin(10, 30, 5, 5);
-    refresh();
-
-    /* Greeting message */
-    wprintw(win, "Hello, Welcome to Mybank.\nWith us, Banking has been made easy.\n\n");
-    wrefresh(win);
-
-    /* Create an account - Implementing registration */
     char firstname[50], lastname[50];
+    char pass[20], val[20];
+    int value;
+    int i = 3;
 
     wprintw(win, "Please create an account\nEnter your first and last names here: ");
     wscanw(win, "%s %s", firstname, lastname);
-
-    char pass[20];
-    char val[20];
-    int value;
-    int i = 3;
-    int passwordCorrect = 0; // Flag var to track correct password validation.
 
     wprintw(win, "Create a password: ");
     wscanw(win, "%s", pass);
@@ -52,8 +33,7 @@ int main()
 
             if (value == 0)
             {
-                passwordCorrect = 1;
-                break;
+                return 1; // Password validation successful
             }
 
             if (i > 0)
@@ -65,52 +45,47 @@ int main()
     }
     else
     {
-        passwordCorrect = 1;
+        return 1; // Password validation successful
     }
 
-    if (passwordCorrect)
-    {
-        wprintw(win, "Welcome to instant banking with My Bank.\nYou have a fixed amount of 50,000\n\n");
-        wrefresh(win);
-
-        while (1)
-        {
-            wprintw(win, "Menu:\n");
-            wprintw(win, "a) Transfer funds\n");
-            wprintw(win, "b) Deposit funds\n");
-            wprintw(win, "c) Check balance\n");
-            wprintw(win, "d) Quit the program\n");
-
-            wprintw(win, "Select an option: ");
-            wscanw(win, " %c", &option);
-
-            switch (option)
-            {
-                case 'a':
-                    wprintw(win, "Option a: Transfer Money\n");
-                    /* Add code to transfer money */
-                    break;
-
-                case 'b':
-                    wprintw(win, "Option b: Deposit Money\n");
-                    /* Add code to deposit money */
-                    break;
-
-                case 'c':
-                    wprintw(win, "Option c: Check balance\n");
-                    /* Add code to check balance */
-                    break;
-
-                case 'd':
-                    wprintw(win, "Option d: Quit the program\n");
-                    endwin();
-                    return 0;
-            }
-
-            wrefresh(win);
-        }
-    }
-
-    endwin();
-    return 0;
+    return 0; // Password validation failed
 }
+
+// Function to handle the main menu options
+void handleMenuOption(WINDOW *win, char option)
+{
+    switch (option)
+    {
+        case 'a':
+            wprintw(win, "Option a: Transfer Money\n");
+            /* Add code to transfer money */
+            break;
+
+        case 'b':
+            wprintw(win, "Option b: Deposit Money\n");
+            /* Add code to deposit money */
+            break;
+
+        case 'c':
+            wprintw(win, "Option c: Check balance\n");
+            /* Add code to check balance */
+            break;
+
+        case 'd':
+            wprintw(win, "Option d: Quit the program\n");
+            endwin();
+            break;
+
+        default:
+            wprintw(win, "Invalid option selected\n");
+            break;
+    }
+}
+
+int main()
+{
+    char option;
+
+    /* Initialize ncurses */
+    initscr();
+    cbreak
